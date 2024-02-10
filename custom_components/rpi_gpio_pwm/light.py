cc722075449aea315610a27747a59ed05297b69c
RedMeKool <58143898+RedMeKool@users.noreply.github.com>
2024-02-10 21:40:51 +0100

    Update light.py

--- custom_components/rpi_gpio_pwm/light.py
+++ custom_components/rpi_gpio_pwm/light.py
@@ -103,7 +103,7 @@ class PwmSimpleLed(LightEntity, RestoreEntity):
     def name(self):
         """Return the name of the group."""
         return self._name
-        
+
     @property
     def unique_id(self):
         return self._unique_id
