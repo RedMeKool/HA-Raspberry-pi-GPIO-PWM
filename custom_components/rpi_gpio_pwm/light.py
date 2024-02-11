fbd44efe947937010571fd4944e26c9631dbf511
RedMeKool <58143898+RedMeKool@users.noreply.github.com>
2024-02-11 15:50:16 +0100

    Update light.py

--- custom_components/rpi_gpio_pwm/light.py
+++ custom_components/rpi_gpio_pwm/light.py
@@ -68,7 +68,10 @@ def setup_platform(
         if CONF_FREQUENCY in led_conf:
             opt_args["frequency"] = led_conf[CONF_FREQUENCY]
         opt_args["pin_factory"] = PiGPIOFactory(host=led_conf[CONF_HOST], port= led_conf[CONF_PORT])
-        led = PwmSimpleLed(PWMLED(pin, **opt_args), led_conf[CONF_NAME], led_conf[CONF_UNIQUE_ID])
+        if CONF_UNIQUE_ID in led_conf:
+            led = PwmSimpleLed(PWMLED(pin, **opt_args), led_conf[CONF_NAME], led_conf[CONF_UNIQUE_ID])
+        else:
+            led = PwmSimpleLed(PWMLED(pin, **opt_args), led_conf[CONF_NAME])
         leds.append(led)
 
     add_entities(leds)
